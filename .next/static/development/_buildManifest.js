2afe60c2cd4e7fa8469fc7e4d16d9537d81de32d
SUDARSAN PERUMAL V <sudarsanperumalv@SUDARSANs-Air.Dlink>
2022-09-15 23:21:41 +0530

    First commit

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/PracticeuseEffect":["static\u002Fchunks\u002Fpages\u002FPracticeuseEffect.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FPracticeuseEffect","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BookList from './PracticeuseState'
+
+ const index = () => {
+  return (
+    <div>
+       <BookList/>
+    </div>
+  )
+}
+
+export default index
